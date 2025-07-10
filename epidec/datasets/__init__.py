f25f7cb149e0b54a422a8e43014494c349846edf
BREW <81044362+b-re-w@users.noreply.github.com>
2025-07-10 12:09:54 +0900

    Feat: Add BalancedDataLoader

--- epidec/datasets/__init__.py
+++ epidec/datasets/__init__.py
@@ -1,1 +1,17 @@
 from .swuniv_dacon import SWUnivDaconDataset, BalancedSWUnivDaconDataset
+from torch.utils.data import DataLoader
+
+
+class BalancedDataLoader(DataLoader):
+    """
+    DataLoader that balances the dataset by sampling from each class equally.
+    """
+    def __init__(self, dataset, resample_function=lambda dt: dt.resample(), *args, **kwargs):
+        super().__init__(dataset, *args, **kwargs)
+        self.dataset = dataset
+        self.resample_function = resample_function
+
+    def __iter__(self):
+        if self.resample_function is not None:
+            self.resample_function(self.dataset)
+        return super().__iter__()
